9b0b0736f42759981804d09e7f6c5fa5cc115e91
NiklasOstonen <s2101042@edu.bc.fi>
2023-08-23 10:18:06 +0300

    Lisäsin Mitä käyttäjä haluaa tehdä

--- VarastoTietokantaSovellus/Program.cs
+++ VarastoTietokantaSovellus/Program.cs
@@ -18,14 +18,26 @@ while(true)
     if (input == 1)
     {
         Console.WriteLine("Lisäsit uuden tuotteen:");
+        Console.WriteLine("Tuotteen id:");
+        Console.WriteLine("Tuotten nimi:");
+        Console.WriteLine("Tuotteen hinta:");
+        Console.WriteLine("Varaston Saldo:");
+        string userId = Console.ReadLine();
+        string userTuoteNimi = Console.ReadLine();
+        string userTuoteHinta = Console.ReadLine();
+        string userVarastoSaldo = Console.ReadLine();
+
     }
     else if (input == 2)
     {
         Console.WriteLine("Poistit tuotteen:");
+        Console.WriteLine("Anna Tuotteen Id:");
+        string userId = Console.ReadLine();
     }
     else if (input == 3)
     {
         Console.WriteLine("Tulostit eri tuotteiden määrän:");
+        Console.WriteLine("Anna Tuotteen Id:");
     }
     else if (input == 4)
     {
@@ -34,6 +46,8 @@ while(true)
     else if (input == 5)
     {
         Console.WriteLine("Muokkasit tuotteen nimeä:");
+        Console.WriteLine("Anna Tuotteen Id:");
+        Console.WriteLine("Anna uusi tuotteelle nimi:");
     }
     else if (input == 0)
     {
